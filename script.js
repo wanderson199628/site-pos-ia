7eaebed73caafa47a0176b36309016ea4f65dec8
wanderson199628 <eng.ambiental2077@gmail.com>
2025-05-30 11:56:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-// Script para simular envio do formulário de upload
-document.getElementById('form-upload').addEventListener('submit', function(e) {
-  e.preventDefault();
-  document.getElementById('mensagem').textContent = 'Tarefa enviada com sucesso (simulação).';
-  this.reset();
-});
+document.getElementById('form-upload').onsubmit = function(e) { e.preventDefault(); alert('Tarefa enviada!'); };
